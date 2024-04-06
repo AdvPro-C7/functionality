52bccc054ca6211f9ac35b0982a9ba4057f465ba
fikrirmdhna <fikrirmdhna01@gmail.com>
2024-04-06 19:30:48 +0700

    initialize

---
+++ src/main/java/id/ac/ui/cs/advprog/functionality/FunctionalityApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.functionality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionalityApplication.class, args);
+	}
+
+}
