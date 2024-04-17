f4bf2b6326f460b63a8d4cc58d24c6413f1f9c3a
fikrirmdhna <fikrirmdhna01@gmail.com>
2024-04-17 20:55:47 +0700

    [RED] Create Book and BookList Skeleton Model

---
+++ src/main/java/id/ac/ui/cs/advprog/functionality/model/Book.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.functionality.model;
+
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+    }
+
+    // Getters and setters
+    public String getTitle() {
+    }
+
+    public String getAuthor() {
+    }
+
+
+    public int getYear() {
+    }
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/functionality/model/BookList.java
@@ -0,0 +1,23 @@
+package id.ac.ui.cs.advprog.functionality.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookList {
+    private List<Book> books;
+
+    public BookList() {
+    }
+
+    public List<Book> getBooks() {
+    }
+
+    public Book getBookByIdx(int index) {
+    }
+
+    public boolean isEmpty() {
+    }
+
+    public String getEmptyMessage() {
+    }
+}
