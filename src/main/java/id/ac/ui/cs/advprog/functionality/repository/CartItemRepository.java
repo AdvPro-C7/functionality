a1a48194735f6829a97a2260748c28ab38cc3e18
venedict chen <venedictchen@gmail.com>
2024-04-17 10:10:32 +0700

    [GREEN] Implement CartItem Repository

--- src/main/java/id/ac/ui/cs/advprog/functionality/repository/CartItemRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/functionality/repository/CartItemRepository.java
@@ -1,1 +1,12 @@
+package id.ac.ui.cs.advprog.functionality.repository;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import id.ac.ui.cs.advprog.functionality.model.CartItem;
+
+
+@Repository
+public interface CartItemRepository extends CrudRepository<CartItem, Integer>{
+
+}
