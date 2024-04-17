41315bc6e3e26afdcfbd7912b570aab488828f78
venedict chen <venedictchen@gmail.com>
2024-04-17 18:30:33 +0700

    design pattern facade

--- src/main/java/id/ac/ui/cs/advprog/functionality/controller/CartController.java
+++ src/main/java/id/ac/ui/cs/advprog/functionality/controller/CartController.java
@@ -1,40 +1,71 @@
 package id.ac.ui.cs.advprog.functionality.controller;
-
-import id.ac.ui.cs.advprog.functionality.model.CartItem;
-import id.ac.ui.cs.advprog.functionality.service.CartItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/cartItem")
-public class CartItemController {
+import id.ac.ui.cs.advprog.functionality.model.Cart;
+import id.ac.ui.cs.advprog.functionality.model.CartItem;
+import id.ac.ui.cs.advprog.functionality.service.CartService;
 
-    private final CartItemService cartItemService;
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+
+    private final CartService cartService;
 
     @Autowired
-    public CartItemController(CartItemService cartItemService) {
-        this.cartItemService = cartItemService;
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<CartItem> createCartItem(@RequestBody CartItem cartItem) {
-        CartItem createdCartItem = cartItemService.createCartItem(cartItem);
-        if (createdCartItem != null) {
-            return ResponseEntity.ok(createdCartItem);
+    @PostMapping("/addItem")
+    public ResponseEntity<Cart> addItemToCart(@RequestParam Integer cartId, @RequestBody CartItem cartItem) {
+        Cart cart = cartService.addItemToCart(cartId, cartItem);
+        if (cart != null) {
+            return ResponseEntity.ok(cart);
         } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            return ResponseEntity.notFound().build();
         }
     }
 
-    @PostMapping("/delete")
-    public ResponseEntity<Boolean> deleteCartItem(@RequestBody CartItem cartItem) {
-        boolean deleted = cartItemService.deleteCartItem(cartItem);
+    @PostMapping("/deleteItem")
+    public ResponseEntity<Boolean> deleteItemFromCart(@RequestParam Integer cartId, @RequestBody CartItem cartItem) {
+        boolean deleted = cartService.deleteItemFromCart(cartId, cartItem);
         if (deleted) {
             return ResponseEntity.ok(true);
         } else {
             return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/deleteAllItems")
+    public ResponseEntity<Boolean> deleteAllItemsFromCart(@RequestParam Integer cartId) {
+        boolean deleted = cartService.deleteAllItemsFromCart(cartId);
+        if (deleted) {
+            return ResponseEntity.ok(true);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/findCart")
+    public ResponseEntity<Cart> findCartByUserId(@RequestParam Integer userId) {
+        Cart cart = cartService.findCartByUserId(userId);
+        if (cart != null) {
+            return ResponseEntity.ok(cart);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Cart> createCart(@RequestParam Integer userId) { 
+        Cart cart = cartService.createCart(userId);
+        if (cart != null) {
+            return ResponseEntity.ok(cart);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
--- src/main/java/id/ac/ui/cs/advprog/functionality/controller/CartItemController.java
+++ src/main/java/id/ac/ui/cs/advprog/functionality/controller/CartItemController.java
@@ -3,15 +3,13 @@ package id.ac.ui.cs.advprog.functionality.controller;
 import id.ac.ui.cs.advprog.functionality.model.CartItem;
 import id.ac.ui.cs.advprog.functionality.service.CartItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cartItem")
 public class CartItemController {
-
     private final CartItemService cartItemService;
 
     @Autowired
@@ -20,12 +18,22 @@ public class CartItemController {
     }
 
     @PostMapping("/create")
-    public CartItem createCartItem(@RequestBody CartItem cartItem) {
-        return cartItemService.createCartItem(cartItem);
+    public ResponseEntity<CartItem> createCartItem(@RequestBody CartItem cartItem) {
+        CartItem createdCartItem = cartItemService.createCartItem(cartItem);
+        if (createdCartItem != null) {
+            return ResponseEntity.ok(createdCartItem);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @PostMapping("/delete")
-    public boolean deleteCartItem(@RequestBody CartItem cartItem) {
-        return cartItemService.deleteCartItem(cartItem);
+    public ResponseEntity<Boolean> deleteCartItem(@RequestBody CartItem cartItem) {
+        boolean deleted = cartItemService.deleteCartItem(cartItem);
+        if (deleted) {
+            return ResponseEntity.ok(true);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
