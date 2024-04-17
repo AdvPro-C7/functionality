47dee51bd5e3c1753f1b64ff12a212b68babfba9
venedict chen <venedictchen@gmail.com>
2024-04-17 09:03:40 +0700

    [GREEN] Implement Cart Model

--- src/main/java/id/ac/ui/cs/advprog/functionality/model/Cart.java
+++ src/main/java/id/ac/ui/cs/advprog/functionality/model/Cart.java
@@ -8,7 +8,25 @@ import java.util.List;
 @Entity
 @Table(name = "cart")
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Integer id;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Getter
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<CartItem> cartItems;
+
+    public Cart() {
+    }
+
+    public Cart(User user) {
+        this.user = user;
+    }
 
 
 }
