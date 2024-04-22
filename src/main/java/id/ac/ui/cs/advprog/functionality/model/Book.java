6717f309ffebb1a8bc226442bedcd9a8b98a65ac
Athar Adista <athar0165@gmail.com>
2024-04-22 21:02:46 +0700

    [GREEN] Implement Book model.

--- src/main/java/id/ac/ui/cs/advprog/functionality/model/Book.java
+++ src/main/java/id/ac/ui/cs/advprog/functionality/model/Book.java
@@ -21,12 +21,35 @@ public class Book {
 
     public Book(String id, String title, String author, String publisher, double price, String description,
                 int stock, String publishDate, String isbn, int pages, String coverPicture, String category, int sold){
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.price = price;
+        this.description = description;
+        this.stock = stock;
+        this.publishDate = publishDate;
+        this.isbn = isbn;
+        this.pages = pages;
+        this.coverPicture = coverPicture;
+        this.category = category;
+        this.sold = sold;
     }
 
     public void setSold(int sold) {
+        if (sold < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            this.sold = sold;
+        }
     }
 
     public void setStock(int stock){
+        if (stock <= 0){
+            throw new IllegalArgumentException();
+        } else {
+            this.stock = stock;
+        }
     }
 
 }
