4d36444b6d28cd0681501d1c38ccd9a1495c5eb0
fikrirmdhna <fikrirmdhna01@gmail.com>
2024-04-24 18:30:19 +0700

    [GREEN] Implement Service for BookService and BookServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/functionality/service/BookService.java
+++ src/main/java/id/ac/ui/cs/advprog/functionality/service/BookService.java
@@ -4,4 +4,14 @@ import id.ac.ui.cs.advprog.functionality.model.Book;
 import java.util.List;
 
 public interface BookService {
+    List<Book> findAllBooks();
+    List<Book> searchBooks(String keyword);
+    List<Book> findBooksByNewest();
+    List<Book> findBooksByPopularity();
+    List<Book> findBooksByPriceAsc();
+    List<Book> findBooksByPriceDesc();
+    List<Book> searchAndSortBooks(String keyword, String sortBy);
+    Book getBookByIndex(int index);
+    boolean isRepositoryEmpty();
+    String getEmptyRepositoryMessage();
 }
--- src/main/java/id/ac/ui/cs/advprog/functionality/service/BookServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/functionality/service/BookServiceImpl.java
@@ -14,41 +14,51 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public List<Book> findAllBooks() {
+        return bookRepository.getBooks();
     }
 
     @Override
     public List<Book> searchBooks(String keyword) {
+        return bookRepository.searchBooks(keyword);
     }
 
     @Override
     public List<Book> findBooksByNewest() {
+        return bookRepository.sortBooksByNewest();
     }
 
     @Override
     public List<Book> findBooksByPopularity() {
+        return bookRepository.sortBooksByPopularity();
     }
 
     @Override
     public List<Book> findBooksByPriceAsc() {
+        return bookRepository.sortBooksByPriceAsc();
     }
 
     @Override
     public List<Book> findBooksByPriceDesc() {
+        return bookRepository.sortBooksByPriceDesc();
     }
 
     @Override
     public List<Book> searchAndSortBooks(String keyword, String sortBy) {
+        return bookRepository.searchAndSortBooks(keyword, sortBy);
     }
 
     @Override
     public Book getBookByIndex(int index) {
+        return bookRepository.getBookByIdx(index);
     }
 
     @Override
     public boolean isRepositoryEmpty() {
+        return bookRepository.isEmpty();
     }
 
     @Override
     public String getEmptyRepositoryMessage() {
+        return bookRepository.getEmptyMessage();
     }
 }
