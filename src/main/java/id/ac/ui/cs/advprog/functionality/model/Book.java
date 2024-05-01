18bfd491c3c5b214e588237aa14ec3f161173329
Athar Adista <athar0165@gmail.com>
2024-05-01 13:14:03 +0700

    implement jpa and change id variable type to int

--- src/main/java/id/ac/ui/cs/advprog/functionality/model/Book.java
+++ src/main/java/id/ac/ui/cs/advprog/functionality/model/Book.java
@@ -1,25 +1,66 @@
 package id.ac.ui.cs.advprog.functionality.model;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 
+@Entity
+@Table(name = "book", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"title", "author"})
+})
+
 public class Book {
-    String id;
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
+    int id;
+
+    @Column(name = "title", updatable = false, nullable = false)
+    @Setter(AccessLevel.NONE)
     String title;
+
+    @Column(name = "author", updatable = false, nullable = false)
+    @Setter(AccessLevel.NONE)
     String author;
+
+    @Column(name = "publisher", nullable = false)
     String publisher;
+
+    @Column(name = "price")
     double price;
+
+    @Column(name = "description", nullable = false)
     String description;
+
+    @Column(name = "stock")
     int stock;
+
+    @Column(name = "publish_date", nullable = false)
     String publishDate;
+
+    @Column(name = "isbn", nullable = false)
     String isbn;
+
+    @Column(name = "pages")
     int pages;
+
+    @Column(name = "cover_picture", nullable = false)
     String coverPicture;
+
+    @Column(name = "category", nullable = false)
     String category;
+
+    @Column(name = "sold", nullable = false)
     int sold;
 
-    public Book(String id, String title, String author, String publisher, double price, String description,
+    public Book(){
+
+    }
+
+    public Book(int id, String title, String author, String publisher, double price, String description,
                 int stock, String publishDate, String isbn, int pages, String coverPicture, String category, int sold){
         this.id = id;
         this.title = title;
