2b54c360a39d23172da616e7a5b133a53f077539
Athar Adista <athar0165@gmail.com>
2024-05-08 14:41:50 +0700

    [RED] Add User model skeleton.

---
+++ src/main/java/id/ac/ui/cs/advprog/functionality/model/User.java
@@ -0,0 +1,35 @@
+package id.ac.ui.cs.advprog.functionality.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"email", "no_telp"})
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name= "nama")
+    private String nama;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "no_telp")
+    private String noTelp;
+
+    @Column(name= "password")
+    private String password;
+
+    public User(){
+
+    }
+    public User(String nama, String email, String noTelp, String password) {
+
+    }
+}
