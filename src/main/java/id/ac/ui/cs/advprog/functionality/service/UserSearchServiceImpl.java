68c9ddecbf2b52aab91b74f65caa1ff8c0be0705
Athar Adista <athar0165@gmail.com>
2024-05-08 14:57:16 +0700

    [RED] Add UserSearchServiceImpl skeleton.

---
+++ src/main/java/id/ac/ui/cs/advprog/functionality/service/UserSearchServiceImpl.java
@@ -0,0 +1,32 @@
+package id.ac.ui.cs.advprog.functionality.service;
+
+import id.ac.ui.cs.advprog.functionality.model.User;
+import id.ac.ui.cs.advprog.functionality.repository.UserSearchByEmailRepository;
+import id.ac.ui.cs.advprog.functionality.repository.UserSearchByNameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserSearchServiceImpl implements UserSearchService{
+
+    @Autowired
+    UserSearchByNameRepository userSearchByNameRepository;
+
+    @Autowired
+    UserSearchByEmailRepository userSearchByEmailRepository;
+
+    @Override
+    public List<User> findByEmail (String email){
+        return null;
+    }
+
+    @Override
+    public List<User> findByName (String name){
+        return null;
+    }
+
+
+}
+
