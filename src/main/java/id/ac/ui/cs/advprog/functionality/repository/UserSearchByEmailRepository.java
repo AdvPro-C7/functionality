9405a7bf48546863ae67d304b96c18033b52230c
Athar Adista <athar0165@gmail.com>
2024-05-08 14:50:07 +0700

    [RED] Add UserSearchByEmailRepository skeleton.

---
+++ src/main/java/id/ac/ui/cs/advprog/functionality/repository/UserSearchByEmailRepository.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.functionality.repository;
+
+import id.ac.ui.cs.advprog.functionality.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserSearchByEmailRepository extends JpaRepository<User, String> {
+
+}
