ff8a73411f99b777edcd557470e8d35b3c12fea0
fikrirmdhna <fikrirmdhna01@gmail.com>
2024-05-10 16:35:24 +0700

    [REFACTOR] add books dummy data

--- src/main/java/id/ac/ui/cs/advprog/functionality/repository/BookRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/functionality/repository/BookRepository.java
@@ -14,6 +14,43 @@ import id.ac.ui.cs.advprog.functionality.model.Book;
 public class BookRepository {
     private List<Book> books =  new ArrayList<>();
 
+    public BookRepository() {
+        addDummyBooks();
+    }
+
+    // Method to add dummy books
+    private void addDummyBooks() {
+        books.add(new Book(1, "To Kill a Mockingbird", "Harper Lee", "HarperCollins", 12.99,
+                "To Kill a Mockingbird is a novel by Harper Lee published in 1960. It was immediately successful, winning the Pulitzer Prize, and has become a classic of modern American literature.",
+                100, "July 11, 1960", "9780061120084", 336,
+                "to_kill_a_mockingbird.jpg", "Fiction", 50));
+
+        books.add(new Book(2, "1984", "George Orwell", "Secker & Warburg", 9.99,
+                "1984 is a dystopian social science fiction novel by English novelist George Orwell. It was published on 8 June 1949 by Secker & Warburg as Orwell's ninth and final book completed in his lifetime.",
+                150, "June 8, 1949", "9780451524935", 328,
+                "1984.jpg", "Science Fiction", 70));
+
+        books.add(new Book(3, "The Great Gatsby", "F. Scott Fitzgerald", "Charles Scribner's Sons", 10.50,
+                "The Great Gatsby is a 1925 novel by American writer F. Scott Fitzgerald. Set in the Jazz Age on Long Island, near New York City, the novel depicts first-person narrator Nick Carraway's interactions with mysterious millionaire Jay Gatsby and Gatsby's obsession to reunite with his former lover, Daisy Buchanan.",
+                80, "April 10, 1925", "9780743273565", 218,
+                "the_great_gatsby.jpg", "Classic", 40));
+
+        books.add(new Book(4, "The Great Gats", "F. Scott Fitzgerald", "Charles Scribner's Sons", 11.50,
+                "The Great Gatsby is a 1925 novel by American writer F. Scott Fitzgerald. Set in the Jazz Age on Long Island, near New York City, the novel depicts first-person narrator Nick Carraway's interactions with mysterious millionaire Jay Gatsby and Gatsby's obsession to reunite with his former lover, Daisy Buchanan.",
+                80, "April 10, 1925", "9780743273565", 218,
+                "the_great_gatsby.jpg", "Classic", 40));
+            
+        books.add(new Book(5, "Great Gat", "F. Scott Fitzgerald", "Charles Scribner's Sons", 13.50,
+                "The Great Gatsby is a 1925 novel by American writer F. Scott Fitzgerald. Set in the Jazz Age on Long Island, near New York City, the novel depicts first-person narrator Nick Carraway's interactions with mysterious millionaire Jay Gatsby and Gatsby's obsession to reunite with his former lover, Daisy Buchanan.",
+                80, "April 10, 1925", "9780743273565", 218,
+                "the_great_gatsby.jpg", "Classic", 40));
+            
+        books.add(new Book(6, "Kill Bill", "Harper Lee", "HarperCollins", 9,
+                "To Kill a Mockingbird is a novel by Harper Lee published in 1960. It was immediately successful, winning the Pulitzer Prize, and has become a classic of modern American literature.",
+                100, "July 11, 1960", "9780061120084", 336,
+                "to_kill_a_mockingbird.jpg", "Fiction", 50));
+    }
+
     public List<Book> searchBooks(String keyword) {
         return books.stream()
                     .filter(book -> book.getTitle().toLowerCase().contains(keyword.toLowerCase()) ||
