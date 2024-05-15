773d8fc0b35c1405c1ebc060d5061efcfb33861d
Athar Adista <athar0165@gmail.com>
2024-05-15 07:44:58 +0700

    Add : Connect to Admin service to get Book Details data

--- src/main/java/id/ac/ui/cs/advprog/functionality/controller/BookDetailsController.java
+++ src/main/java/id/ac/ui/cs/advprog/functionality/controller/BookDetailsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
@@ -20,14 +21,20 @@ public class BookDetailsController {
     BookDetailsService bookDetailsService;
 
     @RequestMapping(value = "/api/book-details/{id}", method = RequestMethod.GET)
-    public ResponseEntity getDetailsBook(@PathVariable int id){
+    public ResponseEntity<?> getDetailsBook(@PathVariable int id){
         ResponseEntity responseEntity = null;
         try {
-            Optional<Book> book = bookDetailsService.findById(id);
-            responseEntity = ResponseEntity.ok(book);
+            RestTemplate restTemplate = new RestTemplate();
+            String url = "http://localhost:8080/api/book-details/" + id;
+            ResponseEntity<Book> response = restTemplate.getForEntity(url, Book.class);
+            if(response.getStatusCode().is2xxSuccessful()) {
+                Book book = response.getBody();
+                return ResponseEntity.ok(book);
+            } else {
+                return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+            }
         } catch (Exception e){
-            responseEntity = ResponseEntity.badRequest().body(HttpStatus.INTERNAL_SERVER_ERROR);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
-        return responseEntity;
     }
 }
