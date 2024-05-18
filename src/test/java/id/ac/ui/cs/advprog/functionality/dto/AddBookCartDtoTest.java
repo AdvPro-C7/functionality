cc37c68ff53c62e956dd1eaeae7e952775a1d965
venedictchen <venedictchen@gmail.com>
2024-05-18 16:31:53 +0700

    add coverage addbooktocartdto

--- src/test/java/id/ac/ui/cs/advprog/functionality/dto/AddBookCartDtoTest.java
+++ src/test/java/id/ac/ui/cs/advprog/functionality/dto/AddBookCartDtoTest.java
@@ -58,4 +58,69 @@ public class AddBookCartDtoTest {
         assertThat(dto1.hashCode()).isEqualTo(dto2.hashCode());
         assertThat(dto1.hashCode()).isNotEqualTo(dto3.hashCode());
     }
+    @Test
+    public void testNotEqualsDifferentUserId() {
+        AddBookCartDto dto1 = new AddBookCartDto();
+        dto1.setUserId(1L);
+        dto1.setBookId(100);
+
+        AddBookCartDto dto2 = new AddBookCartDto();
+        dto2.setUserId(2L);
+        dto2.setBookId(100);
+
+        assertThat(dto1).isNotEqualTo(dto2);
+    }
+
+    @Test
+    public void testNotEqualsDifferentBookId() {
+        AddBookCartDto dto1 = new AddBookCartDto();
+        dto1.setUserId(1L);
+        dto1.setBookId(100);
+
+        AddBookCartDto dto2 = new AddBookCartDto();
+        dto2.setUserId(1L);
+        dto2.setBookId(101);
+
+        assertThat(dto1).isNotEqualTo(dto2);
+    }
+
+    @Test
+    public void testEqualsSameObject() {
+        AddBookCartDto dto = new AddBookCartDto();
+        dto.setUserId(1L);
+        dto.setBookId(100);
+
+        assertThat(dto).isEqualTo(dto);
+    }
+
+    @Test
+    public void testNotEqualsNull() {
+        AddBookCartDto dto = new AddBookCartDto();
+        dto.setUserId(1L);
+        dto.setBookId(100);
+
+        assertThat(dto).isNotEqualTo(null);
+    }
+
+    @Test
+    public void testNotEqualsDifferentClass() {
+        AddBookCartDto dto = new AddBookCartDto();
+        dto.setUserId(1L);
+        dto.setBookId(100);
+
+        String otherClassObject = "someString";
+
+        assertThat(dto).isNotEqualTo(otherClassObject);
+    }
+
+    @Test
+    public void testHashCodeConsistency() {
+        AddBookCartDto dto = new AddBookCartDto();
+        dto.setUserId(1L);
+        dto.setBookId(100);
+
+        int initialHashCode = dto.hashCode();
+
+        assertThat(dto.hashCode()).isEqualTo(initialHashCode);
+    }
 }
