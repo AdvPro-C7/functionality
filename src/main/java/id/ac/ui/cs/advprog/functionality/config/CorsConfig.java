0cdb2798b5ef8379389f3746eed0110a8972af94
venedictchen <venedictchen@gmail.com>
2024-05-26 18:24:43 +0700

    cors

--- src/main/java/id/ac/ui/cs/advprog/functionality/config/CorsConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/functionality/config/CorsConfig.java
@@ -2,11 +2,30 @@ package id.ac.ui.cs.advprog.functionality.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig{
+@EnableWebSecurity
+public class CorsConfig {
+
+    public CorsConfig() {
+    }
+
+    @SuppressWarnings("removal")
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests().requestMatchers("/**").permitAll()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
