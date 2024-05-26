77b0bb91122470569ed3ec17467b3f16470dda0d
Athar Adista <athar0165@gmail.com>
2024-05-26 14:18:00 +0700

    [GREEN] Implement new UserSearchController class.

--- src/main/java/id/ac/ui/cs/advprog/functionality/controller/UserSearchController.java
+++ src/main/java/id/ac/ui/cs/advprog/functionality/controller/UserSearchController.java
@@ -17,23 +17,40 @@ public class UserSearchController {
     UserSearchService userSearchService;
 
     @RequestMapping(value = "/users/search", method = RequestMethod.GET)
-    public List<User> searchUsers(@RequestParam(required = false) String nama) {
-        Set<User> users = new HashSet<>();
+    public ResponseEntity<List<User>> searchUsers(@RequestParam(required = false) String nama) {
+        try {
+            Set<User> users = new HashSet<>();
 
-        List<User> userName = userSearchService.findByName(nama);
-        users.addAll(userName);
+            List<User> userName = userSearchService.findByName(nama);
+            users.addAll(userName);
 
-        List<User> userEmail = userSearchService.findByEmail(nama);
-        users.addAll(userEmail);
+            List<User> userEmail = userSearchService.findByEmail(nama);
+            users.addAll(userEmail);
 
-        return new ArrayList<>(users);
+            return ResponseEntity.ok(new ArrayList<>(users));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(null);
+        }
 
     }
 
     @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
-        return null;
+        ResponseEntity responseEntity = null;
+        try {
+            User user = userSearchService.findById(id);
+            if (user == null){
+                responseEntity = ResponseEntity.notFound().build();
+            } else {
+                responseEntity = ResponseEntity.ok(user);
+            }
+        } catch (Exception e) {
+            responseEntity = ResponseEntity.badRequest().body(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return responseEntity;
+
     }
+
 
 
 }
