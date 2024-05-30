d6c576c4a3e8a26758f3439822f5fa4e671acbc1
venedictchen <venedictchen@gmail.com>
2024-05-30 19:36:47 +0700

    miss

--- src/main/java/id/ac/ui/cs/advprog/functionality/FunctionalityApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/functionality/FunctionalityApplication.java
@@ -4,6 +4,7 @@ import id.ac.ui.cs.advprog.functionality.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @Import(CorsConfig.class)
 @EnableAsync
 @Configuration
+@ComponentScan("id.ac.ui.cs.advprog.functionality")
 public class FunctionalityApplication {
 
 	public static void main(String[] args) {
